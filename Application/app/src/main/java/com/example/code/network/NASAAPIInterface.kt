3107b22afe6b0b94590a142ff15032f43bb38b04
Devrath <devrath.dev595@gmail.com>
2021-06-04 17:47:56 +0530

    v

--- Application/app/src/main/java/com/example/code/network/NASAAPIInterface.kt
+++ Application/app/src/main/java/com/example/code/network/NASAAPIInterface.kt
@@ -2,7 +2,7 @@ package com.example.code.network
 
 import retrofit2.http.GET
 
-const val API_KEY = "Sbvm1ly3ZgmeZUeASUaUWG01icWgmnAQI2UyyoXD"
+const val API_KEY = "XysiavmPaRFTpnvnyzgSTodsxKjGALUYVVPbrDZV"
 
 interface NASAAPIInterface {
   @GET("apod?api_key=$API_KEY")
